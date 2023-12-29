12107e997ad2b48d6cc0e77149debc6745c4d6e1
William Luke <williamluke@me.com>
2023-12-29 10:30:06 -0600

    add serializable protocol

---
+++ Sources/Core/Protocols/Codable/AutoSerializable.swift
@@ -0,0 +1,66 @@
+import Combine
+
+/// An object that can be converted into a dictionary from data
+public protocol Serializable: AutoEncodable {
+ static func serialize(_ output: AutoEncoder.Output) throws -> [String: Any]
+}
+
+extension Optional: Serializable where Wrapped: Serializable {
+ public static func serialize(
+  _ output: Wrapped.AutoEncoder.Output
+ ) throws -> [String: Any] {
+  try Wrapped.serialize(output)
+ }
+}
+
+/// An object that can be converted from a dictionary into data
+public protocol Deserializable: AutoDecodable {
+ static func deserialize(_ input: [String: Any]) throws -> AutoDecoder.Input
+}
+
+extension Optional: Deserializable where Wrapped: Deserializable {
+ public static func deserialize(
+  _ input: [String: Any]
+ ) throws -> AutoDecoder.Input {
+  try Wrapped.deserialize(input)
+ }
+}
+
+/// An object that conforms to `Serializable` & `Deserializable`
+public typealias AutoSerializable = Serializable & Deserializable
+
+import class Foundation.PropertyListSerialization
+
+// MARK: PropertyList Conformances
+public extension Serializable where Self: PlistCodable {
+ static func serialize(_ output: AutoEncoder.Output) throws -> [String: Any] {
+  var format: PropertyListSerialization.PropertyListFormat = .xml
+  guard
+   let output = try PropertyListSerialization
+   .propertyList(
+    from: output, options: .mutableContainersAndLeaves, format: &format
+   ) as? [String: Any] else {
+   throw EncodingError
+    .invalidValue(
+     output,
+     EncodingError.Context(
+      codingPath: [],
+      debugDescription:
+      """
+      SerializationError: Invalid data input for \(Self.self)
+      Please ensure that every property can be stored on a property list
+      """
+     )
+    )
+  }
+  return output
+ }
+}
+
+public extension Deserializable where Self: PlistCodable {
+ static func deserialize(_ input: [String: Any]) throws -> AutoDecoder.Input {
+  try PropertyListSerialization.data(
+   fromPropertyList: input, format: .xml, options: .zero
+  )
+ }
+}
