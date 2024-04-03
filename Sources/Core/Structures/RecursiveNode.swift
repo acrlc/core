12c90f59304157d6d41a62153f70b6c40862a9af
William Luke <williamluke@me.com>
2024-04-03 13:41:11 -0500

    modified recursive node

--- Sources/Core/Structures/RecursiveNode.swift
+++ Sources/Core/Structures/RecursiveNode.swift
@@ -17,37 +17,35 @@ public extension Node {
  }
 }
 
-public protocol RecursiveValue {
- associatedtype Next: RecursiveValue
+public protocol RecursiveElement {
+ associatedtype Next: RecursiveElement
  var next: Next? { get }
 }
 
-public protocol ReflectiveValue {
- associatedtype Previous: RecursiveValue
+public protocol ReflectiveElement {
+ associatedtype Previous: RecursiveElement
  var previous: Previous? { get }
 }
 
-public protocol ConstrainedValue {
- associatedtype Start: RecursiveValue
- associatedtype End: RecursiveValue
+public protocol ConstrainedElement {
+ associatedtype Start: RecursiveElement
+ associatedtype End: RecursiveElement
  var start: Start { get }
  var end: End? { get }
 }
 
 /// A recursive index that can access values and other indices to allow
 /// recursion on a base value
-public protocol IndexicalValue:
- Node, RecursiveValue, ReflectiveValue, ConstrainedValue, ExpressibleAsStart {
+public protocol IndexicalElement:
+ Node, RecursiveElement, ReflectiveElement, ConstrainedElement,
+ ExpressibleAsStart {
  /// Refers to the type of index which is `Self`
  typealias Index = Self
- /// A base value for this index type
- typealias Value = Base.Element
- /// An array of base collections that allow values to be read on an index
- typealias Values = [Base]
+ typealias Element = Base.Element
  /// An array of indices used for recursing values
- typealias Indices = [[Self]]
- var values: Values { get set }
- var value: Value { get set }
+ typealias Indices = [Self]
+ var element: Element { get set }
+ var base: Base { get set }
  var indices: Indices { get set }
  /// The first prior, start index for this value
  var start: Index { get set }
@@ -60,266 +58,260 @@ public protocol IndexicalValue:
  init()
 }
 
-public extension IndexicalValue {
- @inlinable static var start: Index { Self() }
+public extension IndexicalElement {
+ @inlinable
+ static var start: Index { Self() }
 }
 
-public extension IndexicalValue {
- @_disfavoredOverload
- var base: Base {
-  get { values[index] }
-  mutating set { values[index] = newValue }
- }
-
- @_disfavoredOverload
- var elements: [Self] {
-  get { indices[index] }
-  mutating set { indices[index] = newValue }
- }
-
- @_disfavoredOverload
- /// The recursive limit of this index
- @inlinable var limit: Base.Index { self.base.endIndex }
- @_disfavoredOverload
- /// The recursive range of this index
- @inlinable var range: Range<Base.Index> { offset ..< limit }
-}
-
-public extension IndexicalValue
- where Base: RangeReplaceableCollection & MutableCollection {
- @_disfavoredOverload
- @_transparent
- var value: Value {
-  get { values[index][offset] }
-  mutating set { values[index][offset] = newValue }
- }
-}
-
-public extension IndexicalValue
- where Base.Index: Strideable, Base.Index.Stride: SignedInteger {
- @inlinable func contains(
-  where condition: @escaping (Value) throws -> Bool
- ) rethrows -> Bool {
-  for index in range where try condition(base[index]) { return true }
-  return false
- }
-
- @inlinable func first(
-  where condition: @escaping (Value) throws -> Bool
- ) rethrows -> Value? {
-  for index in range {
-   let value = base[index]
-   guard try condition(value) else { continue }
-   return value
-  }
-  return nil
- }
-
- @inlinable func index(
-  where condition: @escaping (Value) throws -> Bool
- ) rethrows -> Index? where Base.Index == Int {
-  for index in range {
-   let value = base[index]
-   guard try condition(value) else { continue }
-   return elements[index]
-  }
-  return nil
- }
-}
-
-public extension IndexicalValue {
- @inlinable func forward(
+public extension IndexicalElement {
+ @inlinable
+ func forward(
   _ perform: @escaping (Index) throws -> ()
  ) rethrows {
-  if let next = self.next {
+  if let next {
    try perform(next)
    try next.forward(perform)
   }
  }
 
- @inlinable func forward(
+ @inlinable
+ func forward(
   _ perform: @escaping (Index) async throws -> ()
  ) async rethrows {
-  if let next = self.next {
+  if let next {
    try await perform(next)
    try await next.forward(perform)
   }
  }
 
- @inlinable func reverse(
+ @inlinable
+ func reverse(
   _ perform: @escaping (Index) throws -> ()
  ) rethrows {
-  while let previous = self.previous { try perform(previous) }
+  while let previous {
+   try perform(previous)
+  }
  }
 
- @inlinable func reverse(
+ @inlinable
+ func reverse(
   _ perform: @escaping (Index) async throws -> ()
  ) async rethrows {
-  while let previous = self.previous { try await perform(previous) }
+  while let previous {
+   try await perform(previous)
+  }
  }
 }
 
 /// Indexical value with storage for rebasing elements
-@frozen public struct UnsafeRecursiveNode<
+@frozen
+public struct UnsafeRecursiveNode<
  Base: RangeReplaceableCollection & MutableCollection
->: IndexicalValue where Base.Index == Int {
+>: IndexicalElement where Base.Index == Int {
  // - MARK: Starting properties
 
- @inlinable public init() {}
+ @inlinable
+ public init() {}
 
  public var index: Int = .zero
- public var offset: Values.Index = .zero
- public var startIndex: Values.Index = .zero
- 
- public var _values: UnsafeMutableRawBufferPointer?
+ public var offset: Indices.Index = .zero
+ public var startIndex: Int = .zero
 
- public var values: Values {
+ public var _base: UnsafeMutablePointer<Base>?
+
+ public var base: Base {
   unsafeAddress {
-   UnsafePointer(
-    self._values.unsafelyUnwrapped
-     .assumingMemoryBound(to: Values.self).baseAddress.unsafelyUnwrapped
-   )
+   UnsafePointer(_base.unsafelyUnwrapped)
   }
   nonmutating unsafeMutableAddress {
-   self._values.unsafelyUnwrapped
-    .assumingMemoryBound(to: Values.self).baseAddress.unsafelyUnwrapped
+   _base.unsafelyUnwrapped
   }
  }
 
- public var _indices: UnsafeMutableRawBufferPointer?
+ public var _indices: UnsafeMutablePointer<Indices>?
 
  public var indices: Indices {
   unsafeAddress {
-   UnsafePointer(
-    self._indices.unsafelyUnwrapped
-     .assumingMemoryBound(to: Indices.self).baseAddress.unsafelyUnwrapped
-   )
+   UnsafePointer(_indices.unsafelyUnwrapped)
   }
   nonmutating unsafeMutableAddress {
-   self._indices.unsafelyUnwrapped
-    .assumingMemoryBound(to: Indices.self).baseAddress.unsafelyUnwrapped
+   _indices.unsafelyUnwrapped
   }
  }
 
- public var value: Value {
-  unsafeAddress {
-   withUnsafePointer(to: self.values[self.index][self.offset]) { $0 }
-  }
-  nonmutating unsafeMutableAddress {
-   withUnsafeMutablePointer(to: &self.values[self.index][self.offset]) { $0 }
-  }
+ public var element: Element {
+  get { base[offset] }
+  nonmutating set { base[offset] = newValue }
  }
 
  /// The value checked, if removed in a separate process, etc.
- public var checkedValue: Value? {
+ public var checkedElement: Element? {
   get {
-   // note: this is an edge case but possible in some use cases
-   assert(self.values.indices.contains(self.index))
    // this can happen when an index is escaped and the value no longer exists
-   guard self.base.indices.contains(self.offset) else { return nil }
-   return self.base[self.offset]
+   guard base.indices.contains(offset) else {
+    return nil
+   }
+   return base[offset]
   }
   nonmutating set {
-   assert(self.values.indices.contains(self.index))
-   guard let newValue, self.base.indices.contains(self.offset) else { return }
-   self.base[self.offset] = newValue
+   guard let newValue, base.indices.contains(offset) else {
+    return
+   }
+   base[offset] = newValue
   }
  }
 }
 
 public extension UnsafeRecursiveNode {
- /// The recursive limit of this index
- var limit: Base.Index { self.base.endIndex }
  /// The recursive range of this index
- var range: Range<Base.Index> { self.offset ..< self.base.endIndex }
-
- var base: Base {
-  unsafeAddress {
-   withUnsafePointer(to: self.values[self.index]) { $0 }
+ var range: Range<Int>? {
+  guard let endIndex else {
+   return nil
   }
-  nonmutating unsafeMutableAddress {
-   withUnsafeMutablePointer(to: &self.values[self.index]) { $0 }
-  }
+  return index ..< endIndex
  }
 
- var elements: [Index] {
-  unsafeAddress {
-   withUnsafePointer(to: self.indices[self.index]) { $0 }
+ /// The recursive limit of this index
+ var limit: Int? { range?.upperBound }
+}
+
+public extension UnsafeRecursiveNode {
+ @inlinable
+ func contains(
+  where condition: @escaping (Element) throws -> Bool
+ ) rethrows -> Bool {
+  guard let range else {
+   return try condition(element)
   }
-  nonmutating unsafeMutableAddress {
-   withUnsafeMutablePointer(to: &self.indices[self.index]) { $0 }
+  for index in range where try condition(base[index]) {
+   return true
   }
+  return false
+ }
+
+ @inlinable
+ func first(
+  where condition: @escaping (Element) throws -> Bool
+ ) rethrows -> Element? {
+  guard let range else {
+   return nil
+  }
+  for index in range {
+   let value = base[index]
+   guard try condition(value) else {
+    continue
+   }
+   return value
+  }
+  return nil
+ }
+
+ @inlinable
+ func index(
+  where condition: @escaping (Element) throws -> Bool
+ ) rethrows -> Index? {
+  guard let range else {
+   return nil
+  }
+  for index in range {
+   let value = base[index]
+   guard try condition(value) else {
+    continue
+   }
+   return indices[index]
+  }
+  return nil
  }
 }
 
 public extension UnsafeRecursiveNode {
  var start: Self {
-  unsafeAddress {
-   withUnsafePointer(to: self.elements[startIndex]) { $0 }
-  }
-  nonmutating unsafeMutableAddress {
-   withUnsafeMutablePointer(to: &self.elements[startIndex]) { $0 }
-  }
- }
-
- var previousIndex: Int? {
-  guard self.offset > base.startIndex else { return nil }
-  return base.index(self.offset, offsetBy: -1)
- }
-
- var nextIndex: Int? {
-  guard offset < base.index(base.endIndex, offsetBy: -1) else { return nil }
-  return base.index(after: self.offset)
- }
-
- var next: Self? {
   get {
-   guard let nextIndex else { return nil }
-   return self.elements[nextIndex]
+   indices[startIndex]
   }
   nonmutating set {
-   guard let newValue, let nextIndex else { return }
-   elements[nextIndex] = newValue
+   self.indices[startIndex] = newValue
   }
+ }
+
+ var previousIndex: Base.Index? {
+  guard index > indices.startIndex else {
+   return nil
+  }
+  return index - 1
  }
 
  var previous: Self? {
   get {
-   guard let previousIndex else { return nil }
-   return self.elements[previousIndex]
+   guard let previousIndex else {
+    return nil
+   }
+   return indices[previousIndex]
   }
   nonmutating set {
-   guard let newValue, let previousIndex else { return }
-   elements[previousIndex] = newValue
+   guard let newValue, let previousIndex else {
+    return
+   }
+   indices[previousIndex] = newValue
   }
  }
 
+ var nextIndex: Base.Index? {
+  guard index < indices.endIndex else {
+   return nil
+  }
+  return index + 1
+ }
+
+ var next: Self? {
+  get {
+   guard let nextIndex else {
+    return nil
+   }
+   return indices[nextIndex]
+  }
+  nonmutating set {
+   guard let newValue, let nextIndex else {
+    return
+   }
+   indices[nextIndex] = newValue
+  }
+ }
+
+ var nextStart: Self? {
+  guard let nextStartIndex else {
+   return nil
+  }
+  return indices[nextStartIndex]
+ }
+
  var endIndex: Int? {
-  guard elements.count > .zero else { return nil }
-  return elements.endIndex
+  guard let nextStartIndex else {
+   return indices.endIndex
+  }
+  return nextStartIndex
  }
 
  var end: Self? {
   get {
-   guard let endIndex else { return nil }
-   return self.elements[endIndex]
+   guard let endIndex else {
+    return nil
+   }
+   return indices[endIndex]
   }
   nonmutating set {
-   guard let newValue, let endIndex else { return }
-   self.elements[endIndex] = newValue
+   guard let newValue, let endIndex else {
+    return
+   }
+   self.indices[endIndex] = newValue
   }
  }
 
  var nextStartIndex: Int? {
-  self.indices.index(
-   start.index, offsetBy: 1,
-   limitedBy: indices.index(indices.endIndex, offsetBy: -1)
-  )
- }
-
- var nextStart: Self? {
-  guard let nextStartIndex else { return nil }
-  return self.indices[nextStartIndex].first
+  guard let nextIndex, index < nextIndex else {
+   return nil
+  }
+  return indices[(index + 1)...].firstIndex(where: { $0.index == .zero })
  }
 }
 
@@ -327,90 +319,58 @@ public extension UnsafeRecursiveNode {
  /// Start here
  static func bind(
   base: Base,
-  values: UnsafeMutablePointer<Values>, indices: UnsafeMutablePointer<Indices>
+  basePointer: UnsafeMutablePointer<Base>,
+  indicesPointer: UnsafeMutablePointer<Indices>
  ) {
-  values.pointee.append(base)
-  indices.pointee.append([Self()])
+  basePointer.pointee.append(contentsOf: base)
+  indicesPointer.pointee.append(Self())
 
-  indices.pointee[0][0]._values =
-   withUnsafeMutableBytes(of: &values.pointee) { $0 }
-  indices.pointee[0][0]._indices =
-   withUnsafeMutableBytes(of: &indices.pointee) { $0 }
+  indicesPointer.pointee[0]._base = basePointer
+  indicesPointer.pointee[0]._indices = indicesPointer
  }
 
  @discardableResult
  /// Start indexing from the current index
- func step(_ content: (Self) throws -> Value?) rethrows -> Value? {
+ func step(_ content: (Self) throws -> Element?) rethrows -> Element? {
   try content(self)
  }
 
- /// Initialize a new start index with a base collection
- @discardableResult
- func start(
-  with base: Base, _ content: (Self) throws -> Value?
- ) rethrows -> Value? {
-  let projection: Self = .start(from: self)
-  let projectedIndex = projection.index
-  self.values.append(base)
-  self.indices.insert([projection], at: projectedIndex)
-
-  if let result = try content(projection) {
-   return result
-  } else {
-   self.values.remove(at: projectedIndex)
-   self.indices.remove(at: projectedIndex)
-   return nil
-  }
- }
-
  /// Add base values to the current index
- func rebase(_ base: Base, _ content: (Self) throws -> Value?) rethrows {
-  for element in base {
-   let projectedIndex = self.elements.endIndex
-   let projectedOffset = self.base.endIndex
-   let projection: Self = .next(with: self)
-   self.base.append(element)
+ func rebase(_ elements: Base, _ content: (Self) throws -> Element?) rethrows {
+  for element in elements {
+   let projectedIndex = indices.endIndex
+   let projectedOffset = base.endIndex
+
+   base.append(element)
+
+   var projection: Self = .next(with: self)
+   projection.index = projectedIndex
+   projection.offset = projectedOffset
 
    if try content(projection) != nil {
-    self.elements.insert(projection, at: projectedIndex)
-   } else {
-    self.base.remove(at: projectedOffset)
+    indices.insert(projection, at: projectedIndex)
+   } else if projectedOffset < base.endIndex {
+    base.remove(at: projectedOffset)
    }
   }
  }
 
- /// Initializes a start index
- static func start(from start: Self) -> Self { Self(from: start) }
- @inlinable init(from start: Self) {
-  self._values = start._values
-  self._indices = start._indices
-  self.index = start.index + 1
- }
-
  /// Initializes the next rebased index
  static func next(with start: Self) -> Self { Self(next: start) }
- @inlinable init(next start: Self) {
-  self._values = start._values
-  self._indices = start._indices
-  self.index = start.index
-  self.startIndex = start.offset
-  // note: think about whether or not this is relevant
-  if let previous {
-   self.offset = previous.offset + 1
-  } else {
-   self.offset = base.endIndex
-  }
+ @inlinable
+ init(next start: Self) {
+  _base = start._base
+  _indices = start._indices
+  startIndex = start.index
  }
 }
 
 import Foundation
 extension UnsafeRecursiveNode: @unchecked Sendable
- where Value: Sendable, Values.Index: Sendable {}
+ where Element: Sendable, Base.Index: Sendable {}
 extension UnsafeRecursiveNode: Hashable {
  public func hash(into hasher: inout Hasher) {
-  hasher.combine(self._values?.baseAddress)
-  hasher.combine(self._indices?.baseAddress)
-  hasher.combine(self.index)
-  hasher.combine(self.offset)
+  hasher.combine(_base)
+  hasher.combine(offset)
  }
 }
