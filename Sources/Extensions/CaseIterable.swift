38d57761b6732369879168bc93b3d36ff85fd155
William Luke <williamluke@me.com>
2024-05-28 22:42:39 -0500

    add previous

--- Sources/Extensions/CaseIterable.swift
+++ Sources/Extensions/CaseIterable.swift
@@ -11,4 +11,17 @@ public extension CaseIterable where Self: Equatable, AllCases.Element == Self {
    return nil
   }
  }
+ 
+ @_disfavoredOverload
+ var previous: AllCases.Element? {
+  guard
+   let index = Self.allCases.firstIndex(of: self)
+  else { return nil }
+  let nextIndex = Self.allCases.index(index, offsetBy: -1)
+  if nextIndex > Self.allCases.startIndex {
+   return Self.allCases[nextIndex]
+  } else {
+   return nil
+  }
+ }
 }
